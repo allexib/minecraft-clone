bc4027e0d868b6bcb6129343ce3da5711550e156
allexib <ooomegasib@gmail.com>
2022-10-10 15:12:24 +0300

    movement

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,16 +1,18 @@
-import { useFrame, useThree } from "@react-three/fiber";
+import { act, useFrame, useThree } from "@react-three/fiber";
 import { useSphere } from "@react-three/cannon";
 import { useEffect, useRef } from "react";
 import { Vector3 } from "three";
 import { useKeyboard } from "../hooks/useKeyboard";
 
+const JUMP_FORCE = 4;
+const SPEED = 4;
+
 export const Player = () => {
-  const actions = useKeyboard();
-  console.log(
-    "actions",
-    Object.entries(actions).filter(([k, v]) => v)
-  );
-  
+  const { moveBackward, moveForvard, moveLeft, moveRight, jump } = useKeyboard();
+  // console.log(
+  //   "actions",
+  //   Object.entries(actions).filter(([k, v]) => v)
+  // );
 
   const { camera } = useThree();
   const [ref, api] = useSphere(() => ({
@@ -33,7 +35,23 @@ export const Player = () => {
     // console.log('frame')
     camera.position.copy(new Vector3(pos.current[0], pos.current[1], pos.current[2]));
 
-    api.velocity.set(0, 1, 0);
+    const direction = new Vector3();
+
+    const frontVector = new Vector3(0, 0, (moveBackward ? 1 : 0) - (moveBackward ? 1 : 0));
+
+    const sideVector = new Vector3((moveLeft ? 1 : 0) - (moveRight ? 1 : 0), 0, 0);
+
+    direction
+      .subVectors(frontVector, sideVector)
+      .normalize()
+      .multiplyScalar(SPEED)
+      .applyEuler(camera.rotation);
+
+    api.velocity.set(direction.x, vel.current[1], direction.z);
+
+    if (jump && Math.abs(vel.current[1] < 0.05)) {
+      api.velocity.set(vel.current[0], JUMP_FORCE, vel.current[2]);
+    }
   });
 
   return <mesh ref={ref}></mesh>;

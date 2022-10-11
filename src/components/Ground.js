38cbcb10cc73699075a0ec79ee6498efddbe5d22
allexib <ooomegasib@gmail.com>
2022-10-11 16:22:58 +0300

    final

--- src/components/Ground.js
+++ src/components/Ground.js
@@ -7,7 +7,7 @@ import { useStore } from "../hooks/useStore";
 export const Ground = () => {
   const [ref] = usePlane(() => ({
     rotation: [-Math.PI / 2, 0, 0],
-    position: [0, 0, 0],
+    position: [0, -0.5, 0],
   }));
   const [addCube] = useStore((state) => [state.addCube]);
 

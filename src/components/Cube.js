b958139a2b5988f1249c53eaca22edff78ddd5f8
allexib <ooomegasib@gmail.com>
2022-10-11 16:16:29 +0300

    hover state on cubes

--- src/components/Cube.js
+++ src/components/Cube.js
@@ -1,8 +1,10 @@
 import { useBox } from "@react-three/cannon";
+import { useState } from "react";
 import { useStore } from "../hooks/useStore";
 import * as textures from "../images/textures";
 
 export const Cube = ({ position, texture }) => {
+  const [isHovered, setIsHovered] = useState(false);
   const [ref] = useBox(() => ({
     type: "Static",
     position,
@@ -15,39 +17,52 @@ export const Cube = ({ position, texture }) => {
 
   return (
     <mesh
+      onPointerMove={(e) => {
+        e.stopPropagation();
+        setIsHovered(true);
+      }}
+      onPointerOut={(e) => {
+        e.stopPropagation();
+        setIsHovered(false);
+      }}
       onClick={(e) => {
         e.stopPropagation();
         const clickedFace = Math.floor(e.faceIndex / 2);
         const { x, y, z } = ref.current.position;
         // console.log('clickedface',clickedFace)
-        if(e.altKey){
-          removeCube(x,y,z)
-          return
-        }
-        else if (clickedFace === 0) {
+        if (e.altKey) {
+          removeCube(x, y, z);
+          return;
+        } else if (clickedFace === 0) {
           addCube(x + 1, y, z);
-          return
+          return;
         } else if (clickedFace === 1) {
           addCube(x - 1, y, z);
-          return
+          return;
         } else if (clickedFace === 2) {
           addCube(x, y + 1, z);
-          return
+          return;
         } else if (clickedFace === 3) {
           addCube(x, y - 1, z);
-          return
+          return;
         } else if (clickedFace === 4) {
-          addCube(x, y, z+1);
-          return
+          addCube(x, y, z + 1);
+          return;
         } else if (clickedFace === 5) {
           addCube(x, y, z - 1);
-          return
+          return;
         }
       }}
       ref={ref}
     >
       <boxBufferGeometry attach="geometry" />
-      <meshStandardMaterial map={activeTexture} attach="material" />
+      <meshStandardMaterial
+        color={isHovered ? "grey" : "white"}
+        map={activeTexture}
+        transparent={true}
+        opacity={texture === "glass" ? 0.6 : 1}
+        attach="material"
+      />
     </mesh>
   );
 };

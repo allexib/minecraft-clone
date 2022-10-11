ce524d1100b2eb92e629170fb7dcc2555007ced3
allexib <ooomegasib@gmail.com>
2022-10-11 15:00:58 +0300

    add/remove cubes

--- src/components/Cube.js
+++ src/components/Cube.js
@@ -1,4 +1,5 @@
 import { useBox } from "@react-three/cannon";
+import { useStore } from "../hooks/useStore";
 import * as textures from "../images/textures";
 
 export const Cube = ({ position, texture }) => {
@@ -7,13 +8,46 @@ export const Cube = ({ position, texture }) => {
     position,
   }));
 
+  const [addCube, removeCube] = useStore((state) => [state.addCube, state.removeCube]);
+
   const activeTexture = textures[texture + "Texture"];
-//   console.log("activeTexture", activeTexture);
+  //   console.log("activeTexture", activeTexture);
 
   return (
-    <mesh ref={ref}>
+    <mesh
+      onClick={(e) => {
+        e.stopPropagation();
+        const clickedFace = Math.floor(e.faceIndex / 2);
+        const { x, y, z } = ref.current.position;
+        // console.log('clickedface',clickedFace)
+        if(e.altKey){
+          removeCube(x,y,z)
+          return
+        }
+        else if (clickedFace === 0) {
+          addCube(x + 1, y, z);
+          return
+        } else if (clickedFace === 1) {
+          addCube(x - 1, y, z);
+          return
+        } else if (clickedFace === 2) {
+          addCube(x, y + 1, z);
+          return
+        } else if (clickedFace === 3) {
+          addCube(x, y - 1, z);
+          return
+        } else if (clickedFace === 4) {
+          addCube(x, y, z+1);
+          return
+        } else if (clickedFace === 5) {
+          addCube(x, y, z - 1);
+          return
+        }
+      }}
+      ref={ref}
+    >
       <boxBufferGeometry attach="geometry" />
-      <meshStandardMaterial map={activeTexture}  attach="material" />
+      <meshStandardMaterial map={activeTexture} attach="material" />
     </mesh>
   );
 };
--- src/hooks/useStore.js
+++ src/hooks/useStore.js
@@ -4,16 +4,16 @@ import { nanoid } from "nanoid";
 export const useStore = create((set) => ({
   texture: "dirt",
   cubes: [
-    {
-      key: nanoid(),
-      pos: [2, 0.5, 0],
-      texture: "dirt",
-    },
-    {
-        key: nanoid(),
-        pos: [1, 0.5, 0],
-        texture: "wood",
-      },
+    // {
+    //   key: nanoid(),
+    //   pos: [2, 0.5, 0],
+    //   texture: "dirt",
+    // },
+    // {
+    //     key: nanoid(),
+    //     pos: [1, 0.5, 0],
+    //     texture: "wood",
+    //   },
   ],
   addCube: (x, y, z) => {
     set((prev) => ({
@@ -27,7 +27,15 @@ export const useStore = create((set) => ({
       ],
     }));
   },
-  removeCube: () => {},
+  removeCube: (x, y, z) => {
+    // console.log('remove',x,y,z)
+    set((prev) => ({
+      cubes: prev.cubes.filter(cube => {
+        const [X, Y, Z] = cube.pos;
+        return X !== x || Y !== y || Z !== z;
+      }),
+    }));
+  },
   setTexture: () => {},
   saveWorld: () => {},
   resetWorld: () => {},
